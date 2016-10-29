9b649ffd69616f899fc6fcf2d5dfb46f43d17d72
徐培堯 <st89382000@gmail.com>
2016-10-29 13:58:35 +0800

    實作引言與分隔線剖析

---
+++ src/SharpMarkdown/Line/Blockquotes.cs
@@ -0,0 +1,38 @@
+﻿using SharpMarkdown.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SharpMarkdown.Line {
+    [Match(Regex = @"^(>\s)+.*")]
+    public class Blockquotes : Content {
+        public int Level { get; set;}
+        public override string OuterMarkdown {
+            get {
+                string result = string.Empty;
+                for (int i = 0; i < Level; i++) result += "> ";
+                return result + Children.Select(x => x.OuterMarkdown);
+            }
+            set {
+                base.OuterMarkdown = value;
+            }
+        }
+
+        public static Blockquotes Parse(string text) {
+            text = text.Trim();
+            var check = MatchAttribute.GetMatchAttributes<Blockquotes>()
+                .Select(x => x.GetRegex().IsMatch(text))
+                .Any();
+            if (!check) throw new FormatException();
+
+            Blockquotes result = new Blockquotes();
+            Regex regex = new Regex(@"(>\s)+");
+            text = text.Replace(regex.Match(text).Value,"");
+            result.Children = Content.InlineParse(text);
+
+            return result;
+        }
+    }
+}
---
+++ src/SharpMarkdown/Line/Divider.cs
@@ -0,0 +1,30 @@
+﻿using SharpMarkdown.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SharpMarkdown.Line {
+    [Match(Regex = @"^([\*\-]\s?){3,}")]
+    public class Divider : ContentBase {
+        public override string OuterMarkdown {
+            get {
+                return "-----";
+            }
+            set {
+                int temp = 0;
+                Parse(value, out temp);
+            }
+        }
+        public static Divider Parse(string text, out int length) {
+            var check = MatchAttribute.GetMatchAttributes<Divider>()
+                .Select(x => x.GetRegex().IsMatch(text))
+                .Any();
+
+            if (!check) throw new FormatException();
+            length = text.Length;
+            return new Divider();
+        }
+
+    }
+}
